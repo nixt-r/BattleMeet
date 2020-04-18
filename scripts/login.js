35e7fd4377737ccab22a1569bb565e7267245068
Tx <63270503+nixt-r@users.noreply.github.com>
2020-04-17 18:15:42 -0600

    Fix archivos y carpetas

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+
+// Sound at Typing
+
+
+function login() {
+        location.href ='../dashboard/dashboard.html';
+        console.log("funciona");
+    }
+
+
+
+
+function load() {
+        var audio = new Audio('../assets/mp3/write.mp3');
+        audio.play();
+        }
---
+++ scripts/register.js
@@ -0,0 +1,13 @@
+
+// Sound at Typing
+function load() {
+    var audio = new Audio('../assets/mp3/write.mp3');
+    audio.play();
+    console.log("funciona");
+    }
+
+
+    function register() {
+        location.href ='../login/login.html';
+        console.log("funciona");
+    }
